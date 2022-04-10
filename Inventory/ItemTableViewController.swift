9ce4a5cbb013c02733df13c193b77d8ebadd58d1
Tri Le <trile95.it@gmail.com>
2022-04-10 19:55:17 -0400

    enable deleting item in item list on items scene

--- Inventory/ItemTableViewController.swift
+++ Inventory/ItemTableViewController.swift
@@ -51,17 +51,16 @@ class ItemTableViewController: UITableViewController {
     }
     */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            itemList.deleteItem(row: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
