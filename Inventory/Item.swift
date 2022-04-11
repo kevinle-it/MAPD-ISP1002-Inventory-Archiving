e0769177af4df4e2a7071253ba7a4763d374677d
Tri Le <trile95.it@gmail.com>
2022-04-10 20:12:45 -0400

    conform item model to NSCoding protocol

--- Inventory/Item.swift
+++ Inventory/Item.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class Item{
+class Item : NSCoding {
     var name: String
     var SKU: String
     var description: String
@@ -18,5 +18,19 @@ class Item{
         self.SKU = SKU
         self.description = description
         self.dateAdded = dateAdded
+    }
+
+    required init?(coder: NSCoder) {
+        name = coder.decodeObject(forKey: "name") as! String
+        SKU = coder.decodeObject(forKey: "SKU") as! String
+        description = coder.decodeObject(forKey: "description") as! String
+        dateAdded = coder.decodeObject(forKey: "dateAdded") as! Date
+    }
+
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: "name")
+        coder.encode(SKU, forKey: "SKU")
+        coder.encode(description, forKey: "description")
+        coder.encode(dateAdded, forKey: "dateAdded")
     }
 }
